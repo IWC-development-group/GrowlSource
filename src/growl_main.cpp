3198529368ae3f430c6b6bc94d3867741863d7b9
Ftoro <lesnik.antonio@gmail.com>
2026-04-19 21:11:30 +0300

    fix: locale fast fix

--- src/growl_main.cpp
+++ src/growl_main.cpp
@@ -8,6 +8,44 @@
 #include "session.h"
 #include "instances.h"
 
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
+std::string ConvertCP1251ToUTF8(const std::string& str)
+{
+	int len = MultiByteToWideChar(1251, 0, str.c_str(), -1, NULL, 0);
+	wchar_t* wstr = new wchar_t[len];
+	MultiByteToWideChar(1251, 0, str.c_str(), -1, wstr, len);
+
+	len = WideCharToMultiByte(CP_UTF8, 0, wstr, -1, NULL, 0, 0, 0);
+	char* utf8 = new char[len];
+	WideCharToMultiByte(CP_UTF8, 0, wstr, -1, utf8, len, 0, 0);
+
+	std::string result(utf8);
+	delete[] wstr;
+	delete[] utf8;
+
+	return result;
+}
+
+std::string ConvertUTF8ToCP1251(const std::string& str)
+{
+	int len = MultiByteToWideChar(CP_UTF8, 0, str.c_str(), -1, NULL, 0);
+	wchar_t* wstr = new wchar_t[len];
+	MultiByteToWideChar(CP_UTF8, 0, str.c_str(), -1, wstr, len);
+
+	len = WideCharToMultiByte(1251, 0, wstr, -1, NULL, 0, NULL, NULL);
+	char* cp1251 = new char[len];
+	WideCharToMultiByte(1251, 0, wstr, -1, cp1251, len, NULL, NULL);
+
+	std::string result(cp1251);
+	delete[] wstr;
+	delete[] cp1251;
+
+	return result;
+}
+
 class TrackAddedEvent : public Event<const Track&> {
 public:
 	TrackAddedEvent() {}
@@ -56,7 +94,7 @@ private:
 
 		for (int i = 0; i < pathCount; i++) {
 			Track track;
-			track.path = paths[i];
+			track.path = ConvertUTF8ToCP1251(paths[i]);
 			event->fire(track);
 		}
 	}
@@ -208,8 +246,8 @@ public:
 				ImGui::OpenPopup("Edit track");
 			}
 
-			ImGui::SameLine();
-			ImGui::Button("Delete");
+			//ImGui::SameLine();
+			//ImGui::Button("Delete");
 
 			processAdditionModal();
 			processEditModal();
@@ -279,6 +317,13 @@ public:
 };
 
 int main() {
+#ifdef _WIN32
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+#else
+	setlocale(LC_ALL, "ru_RU.UTF-8");
+#endif
+
 	Application* growlApp = new Growl();
 	growlApp->run(64);
 	delete growlApp;
